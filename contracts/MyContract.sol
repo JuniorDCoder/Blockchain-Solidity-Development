02391ee13b8f5085547f54b2bc6779f853007d2f
JuniorDCoder <foryoungjuniorngu@gmail.com>
2025-03-18 02:17:19 +0100

    Hotel Booking contract

--- contracts/MyContract.sol
+++ contracts/MyContract.sol
@@ -1,11 +1,35 @@
 // SPDX-License-Identifier: MIT
-pragma solidity >=0.7.0 <0.9.0;
+
+pragma solidity ^0.8.0;
 
 contract MyContract { 
-    // State Variables vs Local variables
 
-    function getValue() public {
-        uint value = 1;
-        return value;
+  // Conditionals
+
+  address public owner;
+
+  constructor(){
+    owner = msg.sender;
+  }
+
+  uint[] public numbers = [1,2,3,4,5,6,7,8,9,10];
+
+  function isEvenNumber(uint _number) public pure returns (bool) {
+    return (_number % 2 == 0);
+  }
+
+  // Loops
+
+  function countEvenNumbers() public view returns(uint){
+    uint count = 0;
+    for (uint i=0; i<numbers.length ; ++i) {
+        if(isEvenNumber(numbers[i])) count++;
     }
+
+    return count;
+  }
+
+  function isOwner() public view returns(bool){
+    return (msg.sender == owner);
+  }
 }
